435fddaa9daa788755f0c581c320c0aa68fe9cf4
长剑腥腥挂壁 <luchaoet@icloud.com>
2022-09-06 14:48:47 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');// 抽离css的插件
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: 'development', // production
+  entry: './src/scss/index.scss',
   output: {
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -15,13 +15,12 @@ module.exports = {
 
   module: {
     rules: [
-
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
           },
           {
             loader: "resolve-url-loader"
