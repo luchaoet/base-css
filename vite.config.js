8d66bfd5794b52fdb9c9c42b1a7dba3b222b40cb
卢超 <luchao@luchaodeMacBook-Pro.local>
2023-05-07 19:05:40 +0800

    使用vite构建

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    cssMinify: false,
+    lib: {
+      entry: resolve(__dirname, 'scss/index.scss'),
+      name: 'dist',
+      fileName: 'index',
+    },
+    // rollupOptions: {
+    //   input: {
+    //     index: resolve(__dirname, 'scss/index.scss')
+    //   },
+    //   output: {
+    //     dir: 'dist'
+    //   }
+    // }
+  }
+})
